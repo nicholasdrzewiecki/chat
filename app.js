db515ba6f6932f771a8fbae4c2efc5f8d009612e
Nicholas <nicholasdrzewiecki@gmail.com>
2017-08-24 00:49:22 -0700

    Added basic socket.io chat functionality

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var session = require("express-session");
+var MongoStore = require("connect-mongo")(session);
 var app = express();
-
 var port = process.env.PORT || 3000;
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
 
 // Mongoose connection
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/chat");
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error'));
+db.on("error", console.error.bind(console, "connection error"));
 
 // Session middleware
 app.use(session({
-  secret: 'secret',
+  secret: "secret",
   resave: true,
   saveUninitialized: false,
   store: new MongoStore({mongooseConnection: db})
@@ -31,19 +32,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 // Serve static files
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 // View engine
-app.set('view engine', 'pug');
-app.set('views', __dirname + '/views'); // Where to find pug templates
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views"); // Where to find pug templates
 
 // Routes
-var routes = require('./routes/index');
-app.use('/', routes);
+var routes = require("./routes/index");
+app.use("/", routes);
 
 // 404
 app.use(function(request, response, next) {
-  var error = new Error('File Not Found');
+  var error = new Error("File Not Found");
   error.status = 404;
   next(error);
 });
@@ -51,14 +52,21 @@ app.use(function(request, response, next) {
 // Error handler
 app.use(function(error, request, response, next) {
   response.status(error.status || 500);
-  response.render('error', {
+  response.render("error", {
     message: error.message,
     status: error.status,
     error: {}
   });
 });
 
+io.on("connection", function(socket) {
+  socket.on('chat message', function(message) {
+    console.log('message: ' + message);
+    io.emit('chat message', message);
+  });
+});
+
 // listen on port 3000
-app.listen(port, function() {
-  console.log('Listening on port: ' + port);
+server.listen(port, function() {
+  console.log("Listening on port: " + port);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var middleware = require('../middleware');
-var User = require('../models/user');
+var middleware = require("../middleware");
+var User = require("../models/user");
 
 // GET /
-router.get('/', function(request, response, next) {
-  return response.render('index', {title: 'Home'});
+router.get("/", function(request, response, next) {
+  return response.render("index", {title: "Home"});
 });
 
 // Request info from client, response data from user, express onto next function after callback
-router.get('/register', middleware.requiresLogout, function(request, response, next) {
-  return response.render('register', {title: 'Register'});
+router.get("/register", middleware.requiresLogout, function(request, response, next) {
+  return response.render("register", {title: "Register"});
 });
 
-router.post('/register', function(request, response, next) {
+router.post("/register", function(request, response, next) {
   if (request.body.email && request.body.username && request.body.password && request.body.confirm) {
     if (request.body.password !== request.body.confirm) {
       var error = new Error("Passwords do not match");
@@ -27,13 +27,13 @@ router.post('/register', function(request, response, next) {
       password: request.body.password
     };
 
-    // Use schema's 'create' method to insert document into Mongo
+    // Use schema"s "create" method to insert document into Mongo
     User.create(userData, function(error, user) {
       if (error) {
         return next(error)
       } else {
         request.session.userId = user._id;
-        return response.redirect('/account');
+        return response.redirect("/account");
       }
     });
 
@@ -44,11 +44,11 @@ router.post('/register', function(request, response, next) {
   }
 });
 
-router.get('/login', middleware.requiresLogout, function(request, response, next) {
-  return response.render('login', {title: 'Login'});
+router.get("/login", middleware.requiresLogout, function(request, response, next) {
+  return response.render("login", {title: "Login"});
 });
 
-router.post('/login', function(request, response, next) {
+router.post("/login", function(request, response, next) {
   if (request.body.email && request.body.password) {
     User.authenticate(request.body.email, request.body.password, function(error, user) {
       if (error || !user) {
@@ -57,35 +57,35 @@ router.post('/login', function(request, response, next) {
         return next(error);
       } else {
         request.session.userId = user._id;
-        return response.redirect('/account');
+        return response.redirect("/account");
       }
     });
   } else {
-    var error = new Error('Email and password are required');
+    var error = new Error("Email and password are required");
     error.status = 401;
     next(error);
   }
 });
 
-router.get('/logout', function(request, response, next) {
+router.get("/logout", function(request, response, next) {
   if (request.session) {
     request.session.destroy(function(error) {
       if (error) {
         return next(error);
       } else {
-        return response.redirect('/')
+        return response.redirect("/")
       }
     })
   }
 });
 
-router.get('/account', middleware.requiresLogin, function(request, response, next) {
+router.get("/account", middleware.requiresLogin, function(request, response, next) {
   User.findById(request.session.userId).exec(function(error, user) {
     if (error) {
       return next(error);
     } else {
-      return response.render('account', {
-        title: 'Account',
+      return response.render("account", {
+        title: "Account",
         username: user.username,
         email: user.email
       })
@@ -93,13 +93,13 @@ router.get('/account', middleware.requiresLogin, function(request, response, nex
   });
 });
 
-router.get('/chat', middleware.requiresLogin, function(request, response, next) {
+router.get("/chat", middleware.requiresLogin, function(request, response, next) {
   User.find(function(error, result) {
     if (error) {
       return next(error);
     } else {
-      return response.render('chat', {
-        title: 'Chat',
+      return response.render("chat", {
+        title: "Chat",
         users: result
       });
     }
